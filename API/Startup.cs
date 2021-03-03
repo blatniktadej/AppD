18c250e16e9fc98b068c94f23a0b07990d39e2d0
blatniktadej <blatnik.tadej1996@gmail.com>
2021-03-03 12:37:28 +0100

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,11 +33,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+           services.AddApplicationServices(_config);
+           services.AddIdentityServices(_config);
             services.AddCors();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -56,7 +59,7 @@ namespace API
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
